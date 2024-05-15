b8a96ae3a36a3aaa5c89d201dcb920f0c0348789
circleorange <piotrbielski3@gmail.com>
2024-05-15 19:51:42 +0100

    first commit & components

---
+++ src/main/java/com/sstack/springstack/SpringstackApplication.java
@@ -0,0 +1,13 @@
+package com.sstack.springstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringstackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringstackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sstack/springstack/SpringstackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sstack.springstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringstackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
